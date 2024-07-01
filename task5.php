e0e5885dab513317876ca8cf143194c26741f68f
Laman <lamankareem@gmail.com>
2024-07-01 04:22:11 -0700

    task 5

---
+++ task5.php
@@ -0,0 +1,12 @@
+<?php
+$a = 5;
+if($a>0){
+    echo "The variable is more than zero";
+}
+elseif($a < 0){
+    echo "The variable is less than zero";
+}
+else{
+    echo "The variable is zero";
+}
+?> 
