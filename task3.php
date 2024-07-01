1535daabd113dfe7e7e5314cd6400b1eaba84372
Laman <lamankareem@gmail.com>
2024-07-01 04:21:40 -0700

    task 2

---
+++ task3.php
@@ -0,0 +1,15 @@
+<?php
+$name = "lyaman";
+$age = 17;
+$is_student = true;
+
+if($is_student = true) { 
+    echo "I am a student". "<br>" ;
+}
+else {
+    echo "I am not a student" . "<br>";
+}
+echo "My name is:" . $name . "<br>";
+echo "I am" . $age . "years old";
+
+?>
